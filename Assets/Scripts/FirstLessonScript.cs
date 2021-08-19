066998e88fb579b20cdb535b1b126bb28d6591c3
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-08-19 18:56:08 +0300

    +created first lesson script

---
+++ Assets/Scripts/FirstLessonScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace LessonsBasic//namespace objeleri gruplandýrmaya yarar
+{
+    public class FirstLessonScript : MonoBehaviour// MonoBehaviour olduðunda objelere eklenebilir oluyor
+    {
+
+        void Start()
+        {
+
+        }
+
+
+        void Update()
+        {
+
+        }
+    }
+}
+

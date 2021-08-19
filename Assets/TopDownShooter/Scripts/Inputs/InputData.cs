734da548a5702e7c8f5fe572328594ef85b1fe6b
hueseyinsoezer <huseyinsozer97@hotmail.com>
2021-08-19 22:51:55 +0300

    +created: input management system

---
+++ Assets/TopDownShooter/Scripts/Inputs/InputData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TopDownShooter.PlayerInput
+{
+    [CreateAssetMenu(menuName = "TopDownShooter/Input/Input Data")]
+    public class InputData : ScriptableObject
+    {
+        public float Horizontal;
+        public float Vertical;
+    }
+
+}
+
